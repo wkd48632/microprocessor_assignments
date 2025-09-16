18528ae52b9a0ecc56d457f5a0f4121bcf4024c3
wkd48632 <wkd48632@gmail.com>
2025-09-16 18:03:18 +0900

    Update 02.LED_RGB_rotation.c

--- 02.LED_RGB_rotation.c
+++ 02.LED_RGB_rotation.c
@@ -9,13 +9,13 @@ int main(void)
     P2->SEL0 &= 0b11111000;
     P2->SEL1 &= 0b11111000;
     P2->DIR  |= 0b00000111;
-    P2->OUT  &= 0b11111000;
+    P2->OUT  &= 0b11111001;
     
-    int i = 1;
+    int i = 3;
     while ( true ) {
-        P2->OUT = P2->OUT & 0b11111000 | i;
+        P2->OUT ^= i;
         printf("P2->OUT: %2x\n", P2->OUT);
-        i <<= 1; i |= i >> 3;
+        i <<= 1; i = i + i / 8 & 7;
         Clock_Delay1ms(1000);
     }
     return 0;
