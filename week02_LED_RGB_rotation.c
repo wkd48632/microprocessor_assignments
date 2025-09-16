be0327d0138c02af0e62bcdf34236076071b2529
wkd48632 <wkd48632@gmail.com>
2025-09-16 09:52:43 +0900

    Update week02_LED_RGB_rotation.c

--- week02_LED_RGB_rotation.c
+++ week02_LED_RGB_rotation.c
@@ -1,25 +1,17 @@
 #include "msp.h"
 #include "Clock.h"
 #include <stdio.h>
-#define LED_R_ON    P2->OUT|=0b00000001
-#define LED_G_ON    P2->OUT|=0b00000010
-#define LED_B_ON    P2->OUT|=0b00000100
-#define LED_R_OFF   P2->OUT&=0b11111110
-#define LED_G_OFF   P2->OUT&=0b11111101
-#define LED_B_OFF   P2->OUT&=0b11111011
-#define LED_ALL_OFF P2->OUT&=0b11111000
 
 int main(void)
 {
     Clock_Init48MHz();
-    
-    // Init
+
     P2->SEL0 &= 0b11111000;
     P2->SEL1 &= 0b11111000;
     P2->DIR  |= 0b00000111;
     P2->OUT  &= 0b11111000;
     
-    int i;
+    int i = 0b00000001;
     while ( true ) {
         if ( i & 0b00001000 ) i++;
         P2->OUT &= (0b11111000 | i);
