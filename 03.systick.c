a303580a20c1ab84402fe5d25c00490486694ca4
wkd48632 <wkd48632@gmail.com>
2025-09-17 09:54:00 +0900

    Update and rename 03.systick to 03.systick.c

--- 03.systick.c
+++ 03.systick.c
@@ -3,21 +3,25 @@
 #include <stdio.h>
 
 void systick_init(void){
-    SysTick->LOAD = 0x00FFFFFF;
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 48000-1;
+    SysTick->VAL = 0;
     SysTick->CTRL = 0x00000005;
 }
 
 void systick_wait1ms(){
-    //SysTick->LOAD = 48000;
+    printf("###");
     SysTick->VAL = 0;
-    while(SysTick->CTRL & 0x00010000 == 0);
+    while((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
 }
 
 
 void systick_wait1s() {
     int i = 0;
     for ( i = 0; i != 1000; i++ ) {
+        printf("##");
         systick_wait1ms();
+
     }
 }
 
@@ -30,6 +34,7 @@ int main(void)
     P2->DIR  |= 0b00000111;
     P2->OUT  &= 0b11111000;
 
+    systick_init();
     char i = 1;
     while ( 1 ) {
         printf("P2->OUT: %2x\n", P2->OUT);
