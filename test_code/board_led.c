58a74633166e96d2cae747aced545583861feb78
wkd48632 <wkd48632@gmail.com>
2025-09-17 12:00:43 +0900

    Update board_led.c

--- test_code/board_led.c
+++ test_code/board_led.c
@@ -1,4 +1,3 @@
-#include "msp.h"
 #include "define.h"
 
 void systick_wait_ms(int n) {
@@ -15,15 +14,15 @@ int main()
     SET_SYSTICK_FOR_1MS;
     SET_GPIO_OUT_BOARD_LED;
 
-    BOARD_LED_ALL_OFF; printf("Ready"); systick_wait_ms(1000);
-    printf("Red");     systick_wait_ms(1000); BOARD_LED_SET_R; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("Green");   systick_wait_ms(1000); BOARD_LED_SET_G; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("Blue");    systick_wait_ms(1000); BOARD_LED_SET_B; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("Cyan");    systick_wait_ms(1000); BOARD_LED_SET_C; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("Magenta"); systick_wait_ms(1000); BOARD_LED_SET_M; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("Yellow");  systick_wait_ms(1000); BOARD_LED_SET_Y; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    printf("White");   systick_wait_ms(1000); BOARD_LED_SET_W; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
-    BOARD_LED_ALL_OFF; printf("Done."); systick_wait_ms(1000);
+    BOARD_LED_ALL_OFF; printf("Ready\n"); systick_wait_ms(1000);
+    printf("Red\n");        systick_wait_ms(1000); BOARD_LED_SET_R; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("Green\n");      systick_wait_ms(1000); BOARD_LED_SET_G; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("Blue\n");       systick_wait_ms(1000); BOARD_LED_SET_B; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("Cyan\n");       systick_wait_ms(1000); BOARD_LED_SET_C; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("Magenta\n");    systick_wait_ms(1000); BOARD_LED_SET_M; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("Yellow\n");     systick_wait_ms(1000); BOARD_LED_SET_Y; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    printf("White\n");      systick_wait_ms(1000); BOARD_LED_SET_W; systick_wait_ms(1000); BOARD_LED_ALL_OFF;
+    BOARD_LED_ALL_OFF; printf("Done.\n"); systick_wait_ms(1000);
 
     return 0;
 }
