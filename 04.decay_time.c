e52ee2c22af5b2f04b86e5811be7f7c15aafba0c
wkd48632 <wkd48632@gmail.com>
2025-09-24 10:11:56 +0900

    Create 04.decay_time.c

---
+++ 04.decay_time.c
@@ -0,0 +1,71 @@
+#include "msp.h"
+#include "Clock.h"
+
+void systick_wait_ms(int n) {
+    int i = 0;
+    for ( i = 0; i != n; i++ ) {
+        SysTick->VAL = 0;
+        while((SysTick->CTRL & 0x00010000) == 0);
+    }
+}
+
+int main()
+{
+    Clock_Init48MHz();
+
+    // IR LED 0, 2, 4, 6
+    P5->SEL0 &= ~0x08;
+    P5->SEL1 &= ~0x08;
+    P5->DIR  |=  0x08;
+    P5->OUT  &= ~0x08;
+
+    // IR LED 1, 3, 5, 7
+    P9->SEL0 &= ~0x04;
+    P9->SEL1 &= ~0x04;
+    P9->DIR  |=  0x04;
+    P9->OUT  &= ~0x04;
+
+    // IR Sensor 0 ~ 7
+    P7->SEL0 &= ~0xFF;
+    P7->SEL1 &= ~0xFF;
+    P7->DIR  &= ~0xFF;
+
+    P3->SEL0 &= ~0xC0;
+    P3->SEL1 &= ~0xC0;
+    P3->DIR  |=  0xC0;
+    P3->OUT  &= ~0xC0;
+
+
+    P2->SEL0 &= ~0b11000000;
+    P2->SEL1 &= ~0b11000000;
+    P2->DIR  |=  0b11000000;
+    P2->OUT  &= ~0b00111111;
+
+    printf("Done.\n");
+    while (1) {
+        P5->OUT |= 0x08;
+        P9->OUT |= 0x04;
+
+        P7->DIR = 0xFF;
+        P7->OUT = 0xFF;
+
+        Clock_Delay1us(10);
+
+        P7->DIR = 0x00;
+
+        int i;
+        for ( i = 0; i < 10000; i++) {
+            int sensor = P7 -> IN & 0x10;
+            if ( !sensor ) {
+                printf("t = %d\n", i);
+                break;
+            }
+            Clock_Delay1us(1);
+        }
+        P5->OUT &= ~0x08;
+        P9->OUT &= ~0x04;
+        Clock_Delay1ms(10);
+    }
+
+    return 0;
+}
